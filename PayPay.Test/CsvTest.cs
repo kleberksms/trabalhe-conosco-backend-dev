d73908b5c5c77909ea0e09895678491f3fa52f72
Kleber <sistemas.kleber@bellinatiperez.com.br>
2018-04-20 08:15:06 -0300

    add sharedkernel and picpaytest

---
+++ PayPay.Test/CsvTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using PicPay.LoadCsv;
+using PicPay.SharedKernel.Models;
+using Xunit;
+
+namespace PayPay.Test
+{
+    public class CsvTest
+    {
+        private readonly Csv _csv;
+        public CsvTest()
+        {
+            _csv = new Csv();
+        }
+        [Fact]
+        public void LoadTest()
+        {
+            var location = Path.Combine(
+                System.Reflection
+                    .Assembly.GetExecutingAssembly().Location
+                    .Split(new [] { "bin" }, StringSplitOptions.None)[0],"Database","users.csv");
+
+            Assert.IsType<List<User>>(_csv.Load(location));
+        }
+    }
+}
---
+++ PicPay.SharedKernel/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PicPay.SharedKernel.Models
+{
+    public sealed class User
+    {
+        public User(string name, string username)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Username = username;
+        }
+
+        public User(Guid id, string name, string username)
+        {
+            Id = id;
+            Name = name;
+            Username = username;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Username { get; private set; }
+    }
+}
