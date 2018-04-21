250d7f68e2fc5cff1ab926c33abb0843e56ff9cc
Kleber <sistemas.kleber@bellinatiperez.com.br>
2018-04-21 11:20:16 -0300

    Add Model State Errors for PUT, POST and DELETE methods

--- PicPay/Controllers/UsersController.cs
+++ PicPay/Controllers/UsersController.cs
@@ -4,41 +4,46 @@ using PicPay.Application.ViewModel.Post;
 namespace PicPay.Controllers
 {
     [Route("api/[controller]")]
-    public class UsersController : Controller
+    public class UsersController : ApiController
     {
-
         // GET api/users/065d8403-8a8f-484d-b602-9138ff7dedcf
         [HttpGet("{id}")]
         public IActionResult Get(string id)
         {
-            return Ok();
+            return Response();
         }
 
-        // GET /api/users?q=whatever
-        [HttpGet]
-        public IActionResult GetByParams([FromQuery]string q)
+        // GET /api/users/q/{paramter-query}/page/{page}
+        [HttpGet, Route("q/{query}/page/{page}")]
+        public IActionResult GetByParams(string query, int page)
         {
-            return Ok();
+            return Response();
         }
 
         [HttpPost]
         public IActionResult Post([FromBody]UserViewModel user)
         {
-            return Ok();
+            ModelState.AddModelError("NI", "Função não necessária para api de teste seletivo Pic Pay");
+            NotifyModelStateErrors();
+            return Response();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody]UserViewModel user)
         {
-            return Ok();
+            ModelState.AddModelError("NI", "Função não necessária para api de teste seletivo Pic Pay");
+            NotifyModelStateErrors();
+            return Response();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public IActionResult Delete(string id)
         {
-            return Ok();
+            ModelState.AddModelError("NI","Função não necessária para api de teste seletivo Pic Pay");
+            NotifyModelStateErrors();
+            return Response();
         }
     }
 }
